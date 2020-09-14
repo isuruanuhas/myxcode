8b7c4418faf6fc48ce79928834f32214199a0906
Isuru Anuhas <isuruanuhasrajapaksha@gmail.com>
2020-09-15 01:32:10 +0530

    Initial commit

---
+++ NIBM COVID19/NIBM COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM COVID19
+//
+//  Created by isuru rajapaksha on 9/14/2563 BE.
+//  Copyright © 2563 BE RAJAPAKSHA R.W.I.A-COBSCCOMP191P039. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
